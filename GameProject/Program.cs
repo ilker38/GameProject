ad88de1716b32b66dbb0c433bb281a7c90d80987
ilker38 <18251966km>
2021-03-24 20:02:02 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer {Id=1,BirthYear=1985,FirstName="ilker",LastName="Uras",IdentityNumber=99966688845 });
+           
+
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace GameProject
+{
+    class UserValidationManager:IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (
+                gamer.BirthYear == 1985 && gamer.FirstName == "ilker" && gamer.LastName == "Uras" &&
+                gamer.IdentityNumber == 99966688845)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
